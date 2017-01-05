a6d9efb1672a72765f5624e8c3a811f767018e32
Diego Llamas <diegollams@gmail.com>
2017-01-05 11:20:54 -0600

    add dauly validation to product and view changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,13 @@ class Product < ApplicationRecord
   belongs_to :user
   validates :name, :price, :image_url, :user_id, presence: true
   validates :price, numericality: { greater_than: 0 }
+  validate :only_3_for_day
 
+  def only_3_for_day
+    return if user.products.count < 3
+    last = user.products.limit(3).order(created_at: :desc).last
+    if Date.today === last.created_at.to_date
+      errors[:base] << 'You already create 3 products today'
+    end
+  end
 end
